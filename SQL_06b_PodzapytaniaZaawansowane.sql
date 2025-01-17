29868d495a22c4845ab3d976e123057363fae9c8
Blazu6 <114349938+Blazu6@users.noreply.github.com>
2025-01-17 12:21:32 +0100

    Update SQL_06b_PodzapytaniaZaawansowane.sql

--- SQL_06b_PodzapytaniaZaawansowane.sql
+++ SQL_06b_PodzapytaniaZaawansowane.sql
@@ -48,4 +48,155 @@ drugie wyliczy sumę płac w każdym zespole (zbiór rekordów, struktura zbioru
 zespołu, suma płac w zespole). Zapytanie główne ma wykonać dwa połączenia: pierwsze połączy
 zbiory wynikowe obu podzapytań do znalezienia szukanego zespołu, drugie, z tabelą Zespoly,
 uzupełni zbiór wynikowy o nazwę zespołu.
+SELECT Z.NAZWA AS NAZWA, T1.SUMA_PLAC AS MAKS_SUMA_PLAC
+FROM (
+    SELECT MAX(SUMA_PLAC) AS MAKSYMALNA_SUMA_PLAC
+    FROM (
+        SELECT ID_ZESP, SUM(PLACA_POD) AS SUMA_PLAC
+        FROM PRACOWNICY
+        GROUP BY ID_ZESP
+    )
+) T2
+JOIN (
+    SELECT ID_ZESP, SUM(PLACA_POD) AS SUMA_PLAC
+    FROM PRACOWNICY
+    GROUP BY ID_ZESP
+) T1
+ON T1.SUMA_PLAC = T2.MAKSYMALNA_SUMA_PLAC
+JOIN ZESPOLY Z
+ON T1.ID_ZESP = Z.ID_ZESP;
+
+SELECT Z.NAZWA AS NAZWA_ZESPOLU, S.SUMA_PLAC AS NAJWYZSZA_SUMA_PLAC
+FROM (
+    SELECT ID_ZESP, SUM(PLACA_POD) AS SUMA_PLAC
+    FROM PRACOWNICY
+    GROUP BY ID_ZESP
+    HAVING SUM(PLACA_POD) = (
+        SELECT MAX(SUMA_PLAC)
+        FROM (
+            SELECT SUM(PLACA_POD) AS SUMA_PLAC
+            FROM PRACOWNICY
+            GROUP BY ID_ZESP
+        )
+    )
+) S
+JOIN ZESPOLY Z
+ON S.ID_ZESP = Z.ID_ZESP;
+
+
+-- 6. Wyświetl nazwiska i pensje trzech najlepiej zarabiających pracowników. Uporządkuj ich zgodnie z
+wartościami pensji w porządku malejącym. Zastosuj podzapytanie skorelowane. 
+SELECT NAZWISKO, PLACA_POD
+FROM PRACOWNICY P1
+WHERE 3 > (
+    SELECT COUNT(*)
+    FROM PRACOWNICY P2
+    WHERE P2.PLACA_POD > P1.PLACA_POD
+)
+ORDER BY PLACA_POD DESC;
+
+-- 7. Dla każdego pracownika podaj jego nazwisko, płacę podstawową oraz różnicę między jego płacą
+podstawową a średnią płacą podstawową w zespole, do którego pracownik należy. Zaproponuj dwa
+rozwiązania, wykorzystujące: (1) podzapytanie w klauzuli SELECT (2) podzapytanie w klauzuli
+FROM. 
+SELECT 
+    NAZWISKO, 
+    PLACA_POD, 
+    PLACA_POD - (
+        SELECT AVG(PLACA_POD)
+        FROM PRACOWNICY P2
+        WHERE P2.ID_ZESP = P1.ID_ZESP
+    ) AS ROZNICA
+FROM PRACOWNICY P1;
+
+SELECT 
+    P2.NAZWISKO, 
+    P2.PLACA_POD, 
+    P2.PLACA_POD - P1.SREDNIA AS ROZNICA
+FROM
+    (SELECT ID_ZESP, AVG(PLACA_POD) AS SREDNIA
+     FROM PRACOWNICY
+     GROUP BY ID_ZESP) P1
+JOIN
+    PRACOWNICY P2 
+ON 
+    P1.ID_ZESP = P2.ID_ZESP
+ORDER BY P2.NAZWISKO;
+
+-- 8. Ogranicz poprzedni zbiór tylko do tych pracowników, którzy zarabiają więcej niż średnia w ich
+zespole (czyli mających dodatnią wartość różnicy między ich płacą podstawową a średnią płacą w
+ich zespole). Modyfikacji poddaj oba rozwiązania z poprzedniego punktu. 
+SELECT 
+    NAZWISKO, 
+    PLACA_POD, 
+    PLACA_POD - (
+        SELECT AVG(PLACA_POD)
+        FROM PRACOWNICY P2
+        WHERE P2.ID_ZESP = P1.ID_ZESP
+    ) AS ROZNICA
+FROM PRACOWNICY P1
+WHERE PLACA_POD - (
+        SELECT AVG(PLACA_POD)
+        FROM PRACOWNICY P2
+        WHERE P2.ID_ZESP = P1.ID_ZESP
+    ) > 0
+ORDER BY NAZWISKO;
+
+SELECT 
+    P2.NAZWISKO, 
+    P2.PLACA_POD, 
+    P2.PLACA_POD - P1.SREDNIA AS ROZNICA
+FROM
+    (SELECT ID_ZESP, AVG(PLACA_POD) AS SREDNIA
+     FROM PRACOWNICY
+     GROUP BY ID_ZESP) P1
+JOIN
+    PRACOWNICY P2 
+ON 
+    P1.ID_ZESP = P2.ID_ZESP
+WHERE
+    P2.PLACA_POD - P1.SREDNIA > 0
+ORDER BY P2.NAZWISKO;
+
+-- 9. Wyświetl nazwiska profesorów, zatrudnionych na Piotrowie, wraz liczbą ich podwładnych. Wynik
+uporządkuj wg liczby podwładnych w porządku malejącym. Zastosuj podzapytanie w klauzuli
+SELECT. 
+SELECT  
+    NAZWISKO,
+    (SELECT COUNT(*) FROM PRACOWNICY P WHERE P.ID_SZEFA=S.ID_PRAC) AS PODWLADNI
+FROM
+    PRACOWNICY S
+JOIN
+    ZESPOLY Z ON S.ID_ZESP=Z.ID_ZESP 
+WHERE 
+    ETAT = 'PROFESOR' AND ADRES='PIOTROWO 3A'
+
+-- 10. . Dla każdego zespołu wylicz średnią płacę jego pracowników. Następnie porównaj średnią w zespole
+z ogólną średnią płac i odpowiednio oznacz nastroje w zespole: umieść :) jeśli średnia w zespole jest
+wyższa lub równa średniej ogólnej i :( w przeciwnym wypadku. Jeśli zespół nie ma pracowników,
+nastrój oznacz jako nieokreślony używając ???.
+SELECT  
+    NAZWA, 
+    (SELECT AVG(PLACA_POD) FROM PRACOWNICY P WHERE P.ID_ZESP = Z.ID_ZESP) AS SREDNIA_W_ZESPOLE,
+    ROUND((SELECT AVG(PLACA_POD) FROM PRACOWNICY), 2) AS SREDNIA_OGOLNA,
+    CASE 
+        WHEN (SELECT AVG(PLACA_POD) FROM PRACOWNICY P WHERE P.ID_ZESP = Z.ID_ZESP)-ROUND((SELECT AVG(PLACA_POD) FROM PRACOWNICY), 2)>0 THEN ':)'
+        WHEN (SELECT COUNT(*) FROM PRACOWNICY WHERE ID_ZESP=Z.ID_ZESP) = 0 THEN '???'
+        ELSE ':('
+    END AS NASTROJE
+FROM 
+    ZESPOLY Z
+ORDER BY 
+    NAZWA
+
+-- 11.  Wyświetl wszystkie informacje o etatach z tabeli Etaty. Wynik zaprezentuj w porządku malejącym,
+ustalonym przez liczbę pracowników, zatrudnionych na poszczególnych etatach. Jeśli na dwóch lub
+więcej etatach pracowałoby tylu samo pracowników, uporządkuj etaty wg ich nazw. Posłuż się
+podzapytaniem w klauzuli ORDER BY.
+SELECT  
+    NAZWA,  PLACA_MIN, PLACA_MAX
+FROM
+    ETATY E
+ORDER BY
+    (SELECT COUNT(*) FROM PRACOWNICY WHERE ETAT = E.NAZWA) DESC, NAZWA;
 
